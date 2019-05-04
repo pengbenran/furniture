d7bd81e1d3ba7c762cbc7447fe472dd692ea571b
pengbenran <810648616@qq.com>
2019-05-04 11:23:24 +0800

    pbr

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,24 +2,7 @@
   <div class="containt">
     <Header/>
     <!-- banner图 -->
-   <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="2000">
-      <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner" role="listbox" >
-        <div class="item active">
-          <img src="../assets/banner.jpg" alt="..." class="img-responsive">
-        </div>
-        <div class="item">
-          <img src="../assets/banner.jpg" alt="..." class="img-responsive">
-        </div>
-        <div class="item">
-          <img src="../assets/banner.jpg" alt="..." class="img-responsive">
-        </div>
-      </div>
-    </div>
+    <Banner/>
     <div class="container">
       <!-- 关于我们 -->
       <div class="row magTop">
@@ -147,8 +130,9 @@
 </template>
 <script>
 import Header from "@/components/public/nav";
+import Banner from "@/components/public/banner";
 export default {
-  components:{Header},
+  components:{Header,Banner},
   name: 'Home',
   data () {
     return {

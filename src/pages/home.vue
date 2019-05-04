d679f579e183e8b3e0ac20384efd9383256da589
SimpleMD <15623140205@163.com>
2019-05-04 11:36:17 +0800

    md

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,29 +2,8 @@
   <div class="containt">
     <Header/>
     <!-- banner图 -->
-<<<<<<< HEAD
-   <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="2000">
-      <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner" role="listbox" >
-        <div class="item active">
-          <img src="../assets/banner.jpg" alt="..." class="img-responsive">
-        </div>
-        <div class="item">
-          <img src="../assets/banner.jpg" alt="..." class="img-responsive">
-        </div>
-        <div class="item">
-          <img src="../assets/banner.jpg" alt="..." class="img-responsive">
-        </div>
-      </div>
-    </div>
-
-=======
     <Banner/>
->>>>>>> d7bd81e1d3ba7c762cbc7447fe472dd692ea571b
+
     <div class="container">
       <!-- 关于我们 -->
       <div class="row magTop">
@@ -121,15 +100,11 @@
 </template>
 <script>
 import Header from "@/components/public/nav";
-<<<<<<< HEAD
 import Footer from "@/components/public/footer";
-export default {
-  components:{Header,Footer},
-=======
 import Banner from "@/components/public/banner";
 export default {
-  components:{Header,Banner},
->>>>>>> d7bd81e1d3ba7c762cbc7447fe472dd692ea571b
+  components:{Header,Banner,Footer},
+
   name: 'Home',
   data () {
     return {
